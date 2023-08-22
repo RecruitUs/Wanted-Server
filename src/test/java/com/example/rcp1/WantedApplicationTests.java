463b60cc919e8140525e7e75eafe91320c3bda3e
LeeJaeHyeok97 <hazardous10@naver.com>
2023-08-23 00:11:09 +0900

    first commit

---
+++ src/test/java/com/example/rcp1/WantedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rcp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
