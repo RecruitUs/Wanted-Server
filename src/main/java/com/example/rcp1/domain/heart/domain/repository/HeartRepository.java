6fffd6bff1aad816126055504f3ce7a82597bf13
LeeJaeHyeok97 <hazardous10@naver.com>
2023-08-30 23:38:19 +0900

    [FIX]#23 연관관계 매핑 수정

--- src/main/java/com/example/rcp1/domain/heart/domain/repository/HeartRepository.java
+++ src/main/java/com/example/rcp1/domain/heart/domain/repository/HeartRepository.java
@@ -1,6 +1,7 @@
 package com.example.rcp1.domain.heart.domain.repository;
 
 import com.example.rcp1.domain.heart.domain.Heart;
+import com.example.rcp1.domain.recruitment.domain.Post;
 import com.example.rcp1.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,5 +11,7 @@ import java.util.Optional;
 public interface HeartRepository extends JpaRepository<Heart, Long> {
 
     Optional<Heart> findByUser(User user);
-    // Optional<Heart> findByUserAndCompany(User user, Company company);
+
+    // Optional<Heart> findByUserAndCompany(User user,  company);
+    Optional<Heart> findByUserAndPost(User user, Post post);
 }
